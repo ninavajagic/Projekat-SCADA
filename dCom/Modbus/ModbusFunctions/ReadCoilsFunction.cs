166b965f682f190971ba31ef08aed07a6f5a7442
ninavajagic <ninchy47@gmail.com>
2024-04-13 20:03:32 +0200

    ReadCoilsFunction

--- dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -24,15 +24,45 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc/>
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] request = new byte[12];
+            ModbusReadCommandParameters mdmRead = this.CommandParameters as ModbusReadCommandParameters;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mdmRead.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mdmRead.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mdmRead.Length)), 0, request, 4, 2);
+            request[6] = mdmRead.UnitId;
+            request[7] = mdmRead.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mdmRead.StartAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mdmRead.Quantity)), 0, request, 10, 2);
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
             //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters mdmRead = this.CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> dict = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            int counter = 0;
+            ushort address = mdmRead.StartAddress;
+            ushort value;
+            byte mask = 1;
+            for (int i = 0; i < response[8]; i++)
+            {
+                byte tempByte = response[9 + i];
+                for (int j = 0; j < 8; j++)
+                {
+                    value = (ushort)(tempByte & mask);
+                    tempByte >>= 1;
+                    dict.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, address), value);
+                    counter++;
+                    address++;
+                    if (counter == mdmRead.Quantity)
+                    {
+                        break;
+                    }
+                }
+            }
+            return dict;
         }
     }
 }
